b99e9398efa139d8c79e17593763ab652aae1b2b
ArtemKoliushko <koliushko.a@gmail.com>
2020-12-06 18:32:14 +0100

    Abstract Game.class added with, GameTest class with tests added

---
+++ src/main/java/com/neven/ticTacToeGame/model/Game.java
@@ -0,0 +1,58 @@
+package com.neven.ticTacToeGame.model;
+
+import lombok.Data;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+@Data
+public abstract class Game {
+
+    private final String player1 = "user_1";
+    private final String player2;
+    private final String emptyCell = "empty";
+    private String currentPlayer;
+
+    private boolean player1win = false;
+    private boolean player2win = false;
+    private boolean draw = false;
+    private boolean incorrectMove = false;
+
+    protected Map<Integer, String> cells = new TreeMap<>();
+
+    public Game(String player_2) {
+        this.player2 = player_2;
+        this.currentPlayer = player1;
+
+        for (int i = 1; i <= 9; i++) {
+            cells.put(i, emptyCell);
+        }
+    }
+
+    protected void switchCurrentPlayer() {
+        if (currentPlayer.equals(player1)) {
+            setCurrentPlayer(player2);
+        } else {
+            setCurrentPlayer(player1);
+        }
+    }
+
+    boolean isPlayerWin(String player) {
+        boolean line_1 = cells.get(1).equals(player) && cells.get(2).equals(player) && cells.get(3).equals(player);
+        boolean line_2 = cells.get(4).equals(player) && cells.get(5).equals(player) && cells.get(6).equals(player);
+        boolean line_3 = cells.get(7).equals(player) && cells.get(8).equals(player) && cells.get(9).equals(player);
+        boolean line_4 = cells.get(1).equals(player) && cells.get(4).equals(player) && cells.get(7).equals(player);
+        boolean line_5 = cells.get(2).equals(player) && cells.get(5).equals(player) && cells.get(8).equals(player);
+        boolean line_6 = cells.get(3).equals(player) && cells.get(6).equals(player) && cells.get(9).equals(player);
+        boolean line_7 = cells.get(1).equals(player) && cells.get(5).equals(player) && cells.get(9).equals(player);
+        boolean line_8 = cells.get(3).equals(player) && cells.get(5).equals(player) && cells.get(7).equals(player);
+
+        return line_1 || line_2 || line_3 || line_4 || line_5 || line_6 || line_7 || line_8;
+    }
+
+    boolean isDraw() {
+        return !cells.containsValue(emptyCell);
+    }
+
+    public abstract Game winningCheckAndMakingMove(int cellNumber);
+}
---
+++ src/test/java/com/neven/ticTacToeGame/model/GameTest.java
@@ -0,0 +1,195 @@
+package com.neven.ticTacToeGame.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameTest {
+
+    private Game testGame;
+
+    @BeforeEach
+    void setUp() {
+        testGame = new Game("test_Player_2") {
+            @Override
+            public Game winningCheckAndMakingMove(int cellNumber) {
+                return null;
+            }
+        };
+    }
+
+    @Test
+    @DisplayName(value = "When call switchCurrentPlayer with currentPlayer player_1 assert that currentPlayer will be changed to player_2")
+    void switchCurrentPlayer1() {
+        testGame.setCurrentPlayer(testGame.getPlayer1());
+        assertEquals(testGame.getPlayer1(), testGame.getCurrentPlayer());
+
+        testGame.switchCurrentPlayer();
+
+        assertEquals(testGame.getPlayer2(), testGame.getCurrentPlayer());
+    }
+
+    @Test
+    @DisplayName(value = "When call switchCurrentPlayer with currentPlayer player_2 assert that currentPlayer will be changed to player_1")
+    void switchCurrentPlayer2() {
+        testGame.setCurrentPlayer(testGame.getPlayer2());
+        assertEquals(testGame.getPlayer2(), testGame.getCurrentPlayer());
+
+        testGame.switchCurrentPlayer();
+
+        assertEquals(testGame.getPlayer1(), testGame.getCurrentPlayer());
+    }
+
+    @Test
+    @DisplayName(value = "When call isDraw method with one or more empty cells assert that method return false")
+    void isDrawFalse() {
+        testGame.getCells().forEach((k, v) -> testGame.getCells().put(k, testGame.getCurrentPlayer()));
+        testGame.getCells().put(1, testGame.getEmptyCell());
+
+        assertFalse(testGame.isDraw());
+    }
+
+    @Test
+    @DisplayName(value = "When call isDraw method with no empty cells assert that method return true")
+    void isDrawTrue() {
+        assertFalse(testGame.isDraw());
+        testGame.getCells().forEach((k, v) -> testGame.getCells().put(k, testGame.getCurrentPlayer()));
+
+        assertTrue(testGame.isDraw());
+    }
+
+    @Test
+    @DisplayName("When call isPlayerWin method with no correct filled lines assert method return false")
+    void isPlayerFail() {
+        assertFalse(testGame.isPlayerWin(testGame.getPlayer1()));
+        assertFalse(testGame.isPlayerWin(testGame.getPlayer2()));
+    }
+
+    @Test
+    @DisplayName("When call isPlayerWin method with correct filled line 1 assert method return true")
+    void isPlayerWinLine1() {
+        putWinningLineInACells(1, 2, 3, testGame.getPlayer1());
+
+        assertTrue(testGame.isPlayerWin(testGame.getPlayer1()));
+        assertFalse(testGame.isPlayerWin(testGame.getPlayer2()));
+
+        testGame.setPlayer1win(false);
+        putWinningLineInACells(1, 2, 3, testGame.getPlayer2());
+
+        assertTrue(testGame.isPlayerWin(testGame.getPlayer2()));
+        assertFalse(testGame.isPlayerWin(testGame.getPlayer1()));
+    }
+
+    @Test
+    @DisplayName("When call isPlayerWin method with correct filled line 1 assert method return true")
+    void isPlayerWinLine2() {
+        putWinningLineInACells(4, 5, 6, testGame.getPlayer1());
+
+        assertTrue(testGame.isPlayerWin(testGame.getPlayer1()));
+        assertFalse(testGame.isPlayerWin(testGame.getPlayer2()));
+
+        testGame.setPlayer1win(false);
+        putWinningLineInACells(4, 5, 6, testGame.getPlayer2());
+
+        assertTrue(testGame.isPlayerWin(testGame.getPlayer2()));
+        assertFalse(testGame.isPlayerWin(testGame.getPlayer1()));
+    }
+
+    @Test
+    @DisplayName("When call isPlayerWin method with correct filled line 1 assert method return true")
+    void isPlayerWinLine3() {
+        putWinningLineInACells(7, 8, 9, testGame.getPlayer1());
+
+        assertTrue(testGame.isPlayerWin(testGame.getPlayer1()));
+        assertFalse(testGame.isPlayerWin(testGame.getPlayer2()));
+
+        testGame.setPlayer1win(false);
+        putWinningLineInACells(7, 8, 9, testGame.getPlayer2());
+
+        assertTrue(testGame.isPlayerWin(testGame.getPlayer2()));
+        assertFalse(testGame.isPlayerWin(testGame.getPlayer1()));
+    }
+
+    @Test
+    @DisplayName("When call isPlayerWin method with correct filled line 1 assert method return true")
+    void isPlayerWinLine4() {
+        putWinningLineInACells(1, 4, 7, testGame.getPlayer1());
+
+        assertTrue(testGame.isPlayerWin(testGame.getPlayer1()));
+        assertFalse(testGame.isPlayerWin(testGame.getPlayer2()));
+
+        testGame.setPlayer1win(false);
+        putWinningLineInACells(1, 4, 7, testGame.getPlayer2());
+
+        assertTrue(testGame.isPlayerWin(testGame.getPlayer2()));
+        assertFalse(testGame.isPlayerWin(testGame.getPlayer1()));
+    }
+
+    @Test
+    @DisplayName("When call isPlayerWin method with correct filled line 1 assert method return true")
+    void isPlayerWinLine5() {
+        putWinningLineInACells(2, 5, 8, testGame.getPlayer1());
+
+        assertTrue(testGame.isPlayerWin(testGame.getPlayer1()));
+        assertFalse(testGame.isPlayerWin(testGame.getPlayer2()));
+
+        testGame.setPlayer1win(false);
+        putWinningLineInACells(2, 5, 8, testGame.getPlayer2());
+
+        assertTrue(testGame.isPlayerWin(testGame.getPlayer2()));
+        assertFalse(testGame.isPlayerWin(testGame.getPlayer1()));
+    }
+
+    @Test
+    @DisplayName("When call isPlayerWin method with correct filled line 1 assert method return true")
+    void isPlayerWinLine6() {
+        putWinningLineInACells(3, 6, 9, testGame.getPlayer1());
+
+        assertTrue(testGame.isPlayerWin(testGame.getPlayer1()));
+        assertFalse(testGame.isPlayerWin(testGame.getPlayer2()));
+
+        testGame.setPlayer1win(false);
+        putWinningLineInACells(3, 6, 9, testGame.getPlayer2());
+
+        assertTrue(testGame.isPlayerWin(testGame.getPlayer2()));
+        assertFalse(testGame.isPlayerWin(testGame.getPlayer1()));
+    }
+
+    @Test
+    @DisplayName("When call isPlayerWin method with correct filled line 1 assert method return true")
+    void isPlayerWinLine7() {
+        putWinningLineInACells(1, 5, 9, testGame.getPlayer1());
+
+        assertTrue(testGame.isPlayerWin(testGame.getPlayer1()));
+        assertFalse(testGame.isPlayerWin(testGame.getPlayer2()));
+
+        testGame.setPlayer1win(false);
+        putWinningLineInACells(1, 5, 9, testGame.getPlayer2());
+
+        assertTrue(testGame.isPlayerWin(testGame.getPlayer2()));
+        assertFalse(testGame.isPlayerWin(testGame.getPlayer1()));
+    }
+
+    @Test
+    @DisplayName("When call isPlayerWin method with correct filled line 1 assert method return true")
+    void isPlayerWinLine8() {
+        putWinningLineInACells(3, 5, 7, testGame.getPlayer1());
+
+        assertTrue(testGame.isPlayerWin(testGame.getPlayer1()));
+        assertFalse(testGame.isPlayerWin(testGame.getPlayer2()));
+
+        testGame.setPlayer1win(false);
+        putWinningLineInACells(3, 5, 7, testGame.getPlayer2());
+
+        assertTrue(testGame.isPlayerWin(testGame.getPlayer2()));
+        assertFalse(testGame.isPlayerWin(testGame.getPlayer1()));
+    }
+
+    private void putWinningLineInACells(int cell1, int cell2, int cell3, String player) {
+        testGame.getCells().put(cell1, player);
+        testGame.getCells().put(cell2, player);
+        testGame.getCells().put(cell3, player);
+    }
+}
