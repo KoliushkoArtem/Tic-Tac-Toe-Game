c6433cef6442d033bfb348e1503a6f995d7a8010
ArtemKoliushko <koliushko.a@gmail.com>
2021-01-13 12:59:25 +0100

    Repositories was refactored. Javadocs for repositories added.

--- src/main/java/com/neven/ticTacToeGame/repository/StatisticRepository.java
+++ src/main/java/com/neven/ticTacToeGame/repository/StatisticRepository.java
@@ -6,8 +6,23 @@ import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+/**
+ * This interface is a repository for {@link Statistic} class.
+ * <br/> This interface gives an opportunity to make different operations with {@link Statistic} class in
+ * database(CRUD and a lot of more).
+ *
+ * @author Arterm Koliushko
+ * @author https://www.linkedin.com/in/artem-koliushko/
+ * @see <a href="https://docs.spring.io/spring-data/jpa/docs/1.5.0.RELEASE/reference/html/jpa.repositories.html">JPA Repositories</a>
+ */
 @Repository
 public interface StatisticRepository extends JpaRepository<Statistic, Long> {
 
+    /**
+     * This method allow to find statistic in database by statistic name.
+     *
+     * @param statisticName {@code String} value for the name of statistic.
+     * @return {@link Optional} with the result of the statistic seeking in the database by statistic name.
+     */
     Optional<Statistic> findByName(String statisticName);
 }
---
+++ src/main/java/com/neven/ticTacToeGame/repository/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * com.neven.ticTacToeGame.repository is a group of interfaces which extends {@link org.springframework.data.jpa.repository.JpaRepository}
+ * interface and is a encapsulating storage for entities of application.
+ */
+package com.neven.ticTacToeGame.repository;
